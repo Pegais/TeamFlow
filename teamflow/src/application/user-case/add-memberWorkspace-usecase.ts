064bac9b4fb9189de06fb6a83fdd503badc98de2
Snehal <snehal9140@gmail.com>
2025-12-18 20:56:41 +0530

    add-workspace-usecase in application layer/addworkspacememeber-command

---
+++ teamflow/src/application/user-case/add-memberWorkspace-usecase.ts
@@ -0,0 +1,58 @@
+//Business flow for this usecase :
+
+/**
+ * Load workspace aggregate root.
+ * call domain method to add member.
+ * pull emmitted domain events.
+ * publishing the events via event bus.
+ * persist workpace (later)
+ */
+
+/**
+ * Remember : application layer do not validate the rules;
+ * it trusts the domain.
+ */
+
+//defining the command object, in DDD we dont pass raw params we give commands.
+import type { WorkspaceRole } from '../../domains/coretruthDomain/user/workspaceDomains/workspace/workspace.types';
+const WorkspaceDomain = require('../../domains/coretruthDomain/user/workspaceDomains/workspace/workspace');
+const eventBus = require('../../domains/observability/domainEvent/eventBus');
+
+
+type AddWorkspaceMemberCommand = {
+    workspaceId: string;
+    actorId: string     //who is performing the action
+    userId: string;     //who is being added to the workspace
+    role: WorkspaceRole;
+}
+
+interface workspaceRepository {
+    findById(id: string): InstanceType<typeof WorkspaceDomain>;
+    save(workspace: InstanceType<typeof WorkspaceDomain>): void;
+}
+
+
+class AddWorkspaceMemberUseCase {
+    constructor(
+        private workspaceRepository: workspaceRepository
+    ) { }
+
+    //public method to add member to workspace
+    public execute(command: AddWorkspaceMemberCommand): void {
+        //load workspace aggregate root
+        const workspace = this.workspaceRepository.findById(command.workspaceId);
+        //call domain method to add member
+        workspace.addMember(command.actorId, command.userId, command.role);
+        //pull emmitted domain events
+        const events = workspace.pullEvents();
+        //publish the events via event bus
+        for (const event of events) {
+            eventBus.publish(event);
+        }
+        //save workspace
+        this.workspaceRepository.save(workspace);
+
+    }
+}
+
+module.exports = AddWorkspaceMemberUseCase;
