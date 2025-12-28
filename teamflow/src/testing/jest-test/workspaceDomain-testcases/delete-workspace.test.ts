4af2e1ffe9c35aba56fe1cb368c4270a95cc55bd
Snehal <snehal9140@gmail.com>
2025-12-28 18:28:14 +0530

    testcase 4: workspace delete event emitted

--- teamflow/src/testing/jest-test/workspaceDomain-testcases/delete-workspace.test.ts
+++ teamflow/src/testing/jest-test/workspaceDomain-testcases/delete-workspace.test.ts
@@ -5,6 +5,7 @@
  * case1  : workspace is not deleted already.
  * case2:workspace can be deleted only by the owner.
  * case 3: workspace should not have any active tasks.
+ * case :workspace should emit a workspace "WORKSPACE_DELETED" event.
  * 
  */
 import WorkspaceDomain from "../../../domains/coretruthDomain/user/workspaceDomains/workspace/workspace";
@@ -58,5 +59,61 @@ describe('workspace should not have any active tasks', () => {
 
         //assertion
         expect(() => testworkspace.deleteWorkspace(ownerId, taskIdStatus)).toThrow('Workspace has active or in progress tasks and cannot be deleted');
+    })
+})
+
+//test case 4 : workspace should emit a workspace "WORKSPACE_DELETED" event.
+class fakeWorkspaceRepository {
+    private workspace :any
+    constructor(workspace:any){
+        this.workspace = workspace;
+    }
+    async findById(id:string):Promise<any>{
+        return this.workspace;
+    }
+    async save(workspace:any):Promise<void>{
+        this.workspace = workspace;
+        console.log('WORKSPACE SAVED :');
+        return Promise.resolve();
+    }
+}
+class fakeSubscriber{
+    handle(event:any):Promise<void>{
+        console.log('EVENT HANDLED :', event.type);
+        return Promise.resolve();
+    }
+}
+//my event subcriber;
+import eventBus from "../../../domains/observability/domainEvent/eventBus";
+
+describe('workspace should emit a workspace "WORKSPACE_DELETED" event', () => {
+    test('should emit a workspace "WORKSPACE_DELETED" event', async() => {
+        //setup ;
+        
+        let ownerId = 'user-1';
+        let taskIdStatus = false;//no active tasks
+        //action
+        const testworkspace = WorkspaceDomain.create(ownerId, 'test workspace', 'test description');
+        
+        const subscriber = new fakeSubscriber();
+       
+        let capturedEvent:any = null;
+        eventBus.subscribe('WORKSPACE_DELETED', async (event:any) => {
+            capturedEvent = event;
+            await subscriber.handle(capturedEvent);
+        });
+        const repository = new fakeWorkspaceRepository(testworkspace);
+        const useCase = new DeleteWorkspaceUseCase(repository);
+        const command = {
+            workspaceId: `${testworkspace['props'].id}`,
+            actorId: ownerId,
+            hasActiveTasks: taskIdStatus
+        }
+        await useCase.execute(command);
+        //assertion
+        // expect(capturedEvent).not.toBeNull();
+        expect(testworkspace['props'].status).toBe('deleted');
+        expect(capturedEvent.type).toBe('WORKSPACE_DELETED');
+       
     })
 })
