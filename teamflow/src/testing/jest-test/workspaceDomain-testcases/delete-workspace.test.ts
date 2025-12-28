8846a0c96e855fc84610e33662506e43952a2007
Snehal <snehal9140@gmail.com>
2025-12-28 17:53:04 +0530

    testcase1 :delete workspace when workspace is already deleted

---
+++ teamflow/src/testing/jest-test/workspaceDomain-testcases/delete-workspace.test.ts
@@ -0,0 +1,31 @@
+/**
+ * Test cases for deleting a workspace
+ * 
+ * test cases :
+ * case1  : workspace is not deleted already.
+ * case2:workspace can be deleted only by the owner.
+ * case 3: workspace should not have any active tasks.
+ * 
+ */
+import WorkspaceDomain from "../../../domains/coretruthDomain/user/workspaceDomains/workspace/workspace";
+
+import DeleteWorkspaceUseCase from "../../../application/use-case/workspace-usecase/delete-workspace-usecase";
+
+//test case 1 : workspace is not deleted already.
+
+describe('delete workspace when the workspace is deleted already', () => {
+    test('should throw error if the workspace is deleted already', () => {
+        //setup ;
+        let workspaceId = 'workspace-1';
+        let ownerId = 'user-1';
+
+        //action
+        const testworkspace = WorkspaceDomain.create(ownerId, 'test workspace', 'test description');
+        testworkspace.deleteWorkspace(ownerId, false);
+        //assertion
+        expect(() => testworkspace.deleteWorkspace(ownerId, false)).toThrow('Workspace is deleted and cannot be modified');
+    })
+})
+// Expected substring: "Workspace is already deleted"
+// Received message:   "Workspace is deleted and cannot be modified"
+//it means our guard of workspace is being triggered.
