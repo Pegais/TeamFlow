e91fcf042349fd4c9e0f21827c84f5a8996113a0
Snehal <snehal9140@gmail.com>
2025-12-29 18:21:58 +0530

    archiveProject-cannot archive deleted project

--- teamflow/src/testing/jest-test/projectDomain/archive-project.test.ts
+++ teamflow/src/testing/jest-test/projectDomain/archive-project.test.ts
@@ -17,3 +17,17 @@ describe("Archive Project", () => {
         expect(project['props'].status).toBe("archived");
     })
 })
+
+/**
+ * cannot archive a project if it is in deleted state.
+ */
+describe("Archive Project", () => {
+    test('cannot archive a project if it is in deleted state',()=>{
+        const project = ProjectDomain.create("Test Project", "123");
+        project.delete(false); //0 active tasks
+
+        //assertions
+        expect(project['props'].status).toBe("deleted");
+        expect(()=>project.archive()).toThrow("Project is deleted");
+    })
+})
