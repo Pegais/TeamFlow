a5b26fb803c155c4747e25496df657ec25a9a439
Snehal <snehal9140@gmail.com>
2025-12-31 19:03:18 +0530

    modifyComment : emits event on updating comment

--- teamflow/src/testing/jest-test/commentDomain/update-comment.test.ts
+++ teamflow/src/testing/jest-test/commentDomain/update-comment.test.ts
@@ -24,3 +24,68 @@ describe("update deleted comment", () => {
         expect(() => comment.edit("this is a new comment")).toThrow("Comment is deleted and cannot be mutated");
     })
 })
+
+
+/**
+ * editing comment should emit COMMENT_UPDATED event.
+ */
+class FakeCommentRepository{
+    private comments:CommentDomain[]=[];
+    constructor(private comment:CommentDomain){
+        this.comments=[this.comment];
+    }
+    public async save(comment:CommentDomain):Promise<void>{
+        this.comments.push(comment);
+        console.log(`Comment saved: ${comment["props"].id}`);
+        return Promise.resolve();
+    }
+    public async findById(id:string):Promise<CommentDomain | null>{
+        const comment = this.comments.find(comment => comment["props"].id === id);
+        if(!comment){
+            return null;
+        }
+        return comment;
+    }
+}
+
+class fakeSubscriber{
+    handler(event:any):Promise<void>{
+        console.log(`Event received: ${event.type}`);
+        return Promise.resolve();
+    }
+}
+
+import eventBus from "../../../domains/observability/domainEvent/eventBus";
+import EditContentUseCase from "../../../application/use-case/comment-usecase/edit-content-usecase";
+
+describe("updateComment", () => {
+    test('should emit COMMENT_UPDATED event', async () => {
+        //setup
+        const comment = CommentDomain.create({
+            id:"123",
+            content:"this is a test comment",
+            taskId:null,
+            projectId:"project-1",
+            authorId:"456",
+            createdAt:new Date(),
+            updatedAt:new Date(),
+            deletedAt:null,
+        });
+        const fakeCommentRepository = new FakeCommentRepository(comment);
+        const subscriber = new fakeSubscriber();
+        //event asubscription
+        let capturedEvent:any=null;
+        eventBus.subscribe("COMMENT_UPDATED",async(event:any)=>{
+            capturedEvent=event;
+            await subscriber.handler(event);
+        });
+        const editContentUseCase = new EditContentUseCase(fakeCommentRepository);
+        await editContentUseCase.execute({
+            commentId:"123",
+            newContent:"this is a new comment",
+        });
+        //assertions
+        expect(capturedEvent).not.toBeNull();
+        expect(capturedEvent.type).toBe("COMMENT_UPDATED");
+    })
+})
